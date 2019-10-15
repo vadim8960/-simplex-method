4399225265bab05ab3282fbf7c1be12e505e8f17
vadim8960 <akimovvadim779@gmail.com>
2019-10-15 03:08:11 +0300

    update more

--- gitop.sh
+++ gitop.sh
@@ -2,12 +2,12 @@
 
 if [ -n "$1" ]
 then
-rm *.gch 
-rm a.out
-git add .
-git commit -m "$*"
-git pull
-git push
+	rm *.gch 
+	rm a.out
+	git add .
+	git commit -m "$*"
+	git pull
+	git push
 else
-echo "Enter commit message. "
+	echo "Enter commit message. "
 fi
